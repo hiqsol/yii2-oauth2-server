483c97989bfe5b0f41fefa39dcbff770bbd8de80
Andrii Vasyliev <sol@hiqdev.com>
2016-05-11 16:31:22 +0000

    added `Module::$options` in a compatible fashion

--- Module.php
+++ Module.php
@@ -47,7 +47,12 @@ class Module extends \yii\base\Module
      * @var array GrantTypes collection
      */
     public $grantTypes = [];
-    
+
+    /**
+     * @var array server options
+     */
+    public $options = [];
+
     /**
      * @var string name of access token parameter
      */
@@ -119,12 +124,12 @@ class Module extends \yii\base\Module
             $server = \Yii::$container->get(Server::className(), [
                 $this,
                 $storages,
-                [
+                array_merge(array_filter([
                     'use_jwt_access_tokens' => $this->useJwtToken,//ADDED
                     'token_param_name' => $this->tokenParamName,
                     'access_lifetime' => $this->tokenAccessLifetime,
                     /** add more ... */
-                ],
+                ]), $this->options),
                 $grantTypes
             ]);
 
